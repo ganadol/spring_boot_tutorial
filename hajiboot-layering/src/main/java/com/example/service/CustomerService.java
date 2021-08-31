66b6f5ecef3c4a2a68abf6b531262076a35fd2e1
night1700 <night1700@gmail.com>
2021-08-31 18:56:21 +0900

    52 page run

---
+++ hajiboot-layering/src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotiation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
